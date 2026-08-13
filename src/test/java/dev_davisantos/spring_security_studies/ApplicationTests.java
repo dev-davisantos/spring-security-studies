444480374f5eb5fbe21f051d4b7d0ea6b5a97001
Davi Santos <davicoutinho984@gmail.com>
2026-08-13 15:18:20 -0300

    chore: initial spring structure

---
+++ src/test/java/dev_davisantos/spring_security_studies/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev_davisantos.spring_security_studies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
