e2942be3e6f3bf474d87874db619f3029a95d4f5
Davi Santos <davicoutinho984@gmail.com>
2026-08-14 08:45:46 -0300

    feat: learn how to add in memory initial users, and how to create a password encoder

--- README.md
+++ README.md
@@ -6,8 +6,15 @@
 ## Commits:
 **Note: I will explain in this readme everything I've learned in each step of the course or tests, using commits to document what i did or learn**
 
-### feat: learn how to create a default securityFilterChain config, and create a controller for testing this:
+### feat: learn how to create a default securityFilterChain config, and create a controller for testing this (#257facb)
 - Learned how default configs of spring security works
 - How to create a simple configuration
 - Created a simple config for route "/public" and others
 - Created a controller to test this routes
+
+### feat: learn how to add in memory initial users, and how to create a password encoder
+- Learned how UserDetailsService works
+- Learned how to create a UserDetailsService with in memory users
+- Learned how Spring Security can authenticated by UserDetails
+- Learned how to create a password encoder
+- Learned how Spring encode and use encoder to matches the password
--- src/main/java/dev_davisantos/spring_security_studies/configuration/SecurityConfig.java
+++ src/main/java/dev_davisantos/spring_security_studies/configuration/SecurityConfig.java
@@ -5,6 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,5 +27,27 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails simpleUser = User.builder()
+                .username("simple-user")
+                .password(passwordEncoder().encode("simple"))
+                .roles("USER")
+                .build();
+
+       UserDetails adminUser = User.builder()
+                .username("admin-user")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(simpleUser, adminUser);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
     }
 }
